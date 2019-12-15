56fd07592cf3e9f9f4e1621792238abc3e72432c
Iain <iain@polevaultweb.com>
2019-12-15 19:44:22 +0000

    Uppercase settings names.

--- includes/settings.php
+++ includes/settings.php
@@ -16,24 +16,26 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Register new settings for the addon.
  *
  * @param array $settings
- * @return void
+ *
+ * @return array
  */
 function wpumul_register_settings( $settings ) {
 
 	$settings['registration'][] = array(
 		'id'   => 'username_min_length',
-		'name' => esc_html__( 'Minimum length', 'wpum-username-length' ),
+		'name' => esc_html__( 'Minimum Length', 'wpum-username-length' ),
 		'desc' => esc_html__( 'Set a minimum characters length for the username - leave blank if not needed.', 'wpum-username-length' ),
 		'type' => 'text',
 	);
 
 	$settings['registration'][] = array(
 		'id'   => 'username_max_length',
-		'name' => esc_html__( 'Maximum length', 'wpum-username-length' ),
+		'name' => esc_html__( 'Maximum Length', 'wpum-username-length' ),
 		'desc' => esc_html__( 'Set a maximum characters length for the username.', 'wpum-username-length' ),
 		'type' => 'text',
 	);
 
 	return $settings;
 }
+
 add_action( 'wpum_registered_settings', 'wpumul_register_settings' );
